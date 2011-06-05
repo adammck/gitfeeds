5bea83c6133759a2c23b0933e2fdf8643c055337
adammck <adam.mckaig@gmail.com>
2011-06-05 04:12:40 -0400

    do something ugly and insane to validate git urls.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "http://rubygems.org"
 
 gem "rails", "3.0.7"
 gem "sqlite3"
+gem "grit"
 
 # avoid rake 0.9 deprecation warnings.
 # (remove when upgrading to rails 3.1.)
--- lib/validators/is_git_url.rb
+++ lib/validators/is_git_url.rb
@@ -3,6 +3,22 @@
 
 class IsGitUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, :not_a_git_url)
+    unless value.blank?
+      unless is_git_url?(value)
+        record.errors.add(attribute, :not_a_git_url)
+      end
+    end
+  end
+
+  private
+
+  # Return true if +url+ appears to be a valid Git URL.
+  def is_git_url?(url, ref="HEAD")
+    out = git.native(:ls_remote, { }, url, ref)
+    out.strip.match /^[0-9a-z]{40}\t#{ref}$/
+  end
+
+  def git
+    Grit::Git.new("/tmp")
   end
 end
